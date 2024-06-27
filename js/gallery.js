efe1eb50daedace660b5e01c6c02d6d014472e1e
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2024-06-27 15:17:34 +0300

    update

--- js/gallery.js
+++ js/gallery.js
@@ -1,7 +1,7 @@
 const images = [
   {
     preview:
-      "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg",
+      "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__340.jpg",
     original:
       "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg",
     description: "Hokkaido Flower",
@@ -63,3 +63,38 @@ const images = [
     description: "Lighthouse Coast Sea",
   },
 ];
+
+const galleryContainer = document.querySelector(".gallery");
+
+const galleryItemsMarkup = images
+  .map(({ preview, original, description }) => {
+    return `
+      <li class="gallery-item">
+        <a class="gallery-link" href="${original}">
+          <img
+            class="gallery-image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>
+    `;
+  })
+  .join("");
+
+galleryContainer.innerHTML = galleryItemsMarkup;
+
+galleryContainer.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") return;
+
+  const source = event.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+      <img src="${source}" width="800" height="600">
+    `);
+
+  instance.show();
+});
